168f1870eaf2e011c0854d484328d8cfe3e9ac42
Bill Wilder <codingoutloud@gmail.com>
2014-01-23 20:46:06 -0500

    update

--- region_meta.py
+++ region_meta.py
@@ -33,5 +33,5 @@ regions = [
     Region("United States", "US South Central", "San Antonio, TX, USA", "US North Central"),
     Region("United States", "US East", "Bristow, Virginia, USA", "US West"),
     Region("United States", "US West", "San Francisco, California, USA", "US East"),
-    Region("United States", "Boston", "Needham, MA, USA", "Europe North"),
+    # fake - for testing only - Region("United States", "Boston", "Needham, MA, USA", "Europe North"),
 ]
