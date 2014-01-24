55d94506501bab92ab6bfae309a2cd2b7b342225
Bill Wilder <codingoutloud@gmail.com>
2014-01-23 20:57:42 -0500

    update

--- build.ps1
+++ build.ps1
@@ -9,9 +9,17 @@ $topo = (Get-Item .\azuremap.topojson).Length
 $perc= [Decimal]::Round(($topo/$geo)*100)
 echo "TopoJSON file size ($topo) is $perc% of GeoJSON file size ($geo)"
 
-echo "[saving back to github]"
+echo "[saving back to azuremap github repo]"
 git commit -a -m "update"
+git push 
+
+echo "[saving map files back to github gist]"
+copy .\azuremap.geojson ..\azuremap.gist
+copy .\azuremap.topojson ..\azuremap.gist
+copy .\azuremap_meta.py ..\azuremap.gist
+pushd ..\azuremap.gist
+git commit -a -m "regenerated from source"
 git push gist master
-git push github master
+popd
 
 echo "[ALL DONE]"
